2f30b882e2766c757dc1f55de13a608d577fe132
unknown <lizzybumpers11@gmail.com>
2023-10-10 09:44:10 -0500

    edited HTML/CSS and started in Script

---
+++ script.js
@@ -0,0 +1,49 @@
+const questions = [
+    {
+        question: "Where in the HTML tags do you add <script>?",
+        answers: [
+            { text: "<header>", correct: false},
+            { text: "<main>", correct: false},
+            { text: "<body>", correct: true},
+            { text: "<head>", correct: false}
+        ]
+    },
+    {
+        question: "Where in the HTML tags do you add <script>?",
+        answers: [
+            { text: "<header>", correct: false},
+            { text: "<main>", correct: false},
+            { text: "<body>", correct: true},
+            { text: "<head>", correct: false}
+        ]
+    },
+    {
+        question: "Where in the HTML tags do you add <script>?",
+        answers: [
+            { text: "<header>", correct: false},
+            { text: "<main>", correct: false},
+            { text: "<body>", correct: true},
+            { text: "<head>", correct: false}
+        ]
+    },
+    {
+        question: "Where in the HTML tags do you add <script>?",
+        answers: [
+            { text: "<header>", correct: false},
+            { text: "<main>", correct: false},
+            { text: "<body>", correct: true},
+            { text: "<head>", correct: false}
+        ]
+    },
+    {
+        question: "Where in the HTML tags do you add <script>?",
+        answers: [
+            { text: "<header>", correct: false},
+            { text: "<main>", correct: false},
+            { text: "<body>", correct: true},
+            { text: "<head>", correct: false}
+        ]
+    }
+];
+let quizQuestion = document.getElementById('question')
+let quizAnswer = document.getElementsByClassName('btn')
