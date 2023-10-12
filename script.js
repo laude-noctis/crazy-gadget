2048e5ef05aef62231450f8c780da6fb273443c3
lizzybumpers11@gmail.com <lizziebumpers@Lizzies-MacBook-Air.local>
2023-10-12 17:10:19 -0500

    starting writing quiz code

--- script.js
+++ script.js
@@ -9,41 +9,43 @@ const questions = [
         ]
     },
     {
-        question: "Where in the HTML tags do you add <script>?",
+        question: "JavaScript is a ____-side programming language?",
         answers: [
-            { text: "<header>", correct: false},
-            { text: "<main>", correct: false},
-            { text: "<body>", correct: true},
-            { text: "<head>", correct: false}
+            { text: "Client", correct: false},
+            { text: "Server", correct: false},
+            { text: "Neither", correct: false},
+            { text: "Client and Server", correct: true}
         ]
     },
     {
-        question: "Where in the HTML tags do you add <script>?",
+        question: "How do you open a confirm window in JavaScript?",
         answers: [
-            { text: "<header>", correct: false},
-            { text: "<main>", correct: false},
-            { text: "<body>", correct: true},
-            { text: "<head>", correct: false}
+            { text: "confirm()", correct: true},
+            { text: "open.confirm()", correct: false},
+            { text: "confirm.open()", correct: false},
+            { text: "openconfirm()", correct: false}
         ]
     },
     {
-        question: "Where in the HTML tags do you add <script>?",
+        question: "Which of the following is used for assigning a value to a variable?",
         answers: [
-            { text: "<header>", correct: false},
-            { text: "<main>", correct: false},
-            { text: "<body>", correct: true},
-            { text: "<head>", correct: false}
+            { text: "*", correct: false},
+            { text: "+", correct: false},
+            { text: "=", correct: true},
+            { text: "-", correct: false}
         ]
     },
     {
-        question: "Where in the HTML tags do you add <script>?",
+        question: "Which symnol is use to target everything in a CSS stylesheet?",
         answers: [
-            { text: "<header>", correct: false},
-            { text: "<main>", correct: false},
-            { text: "<body>", correct: true},
-            { text: "<head>", correct: false}
+            { text: "~", correct: false},
+            { text: "*", correct: true},
+            { text: "&", correct: false},
+            { text: "_", correct: false}
         ]
     }
 ];
 let quizQuestion = document.getElementById('question')
 let quizAnswer = document.getElementsByClassName('btn')
+var questionId = document.getElementById("myElement");
+
